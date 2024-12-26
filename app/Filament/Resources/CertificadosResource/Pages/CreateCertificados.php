35b3aeb3068a92f657b5e7560317eefdd4dc948c
MartinRestrepo97 <palaciosrestrepomartinernesto@gmail.com>
2024-12-26 16:54:52 -0500

    actualizacion_sig_hseq_sas

--- app/Filament/Resources/CertificadosResource/Pages/CreateCertificados.php
+++ app/Filament/Resources/CertificadosResource/Pages/CreateCertificados.php
@@ -9,4 +9,15 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateCertificados extends CreateRecord
 {
     protected static string $resource = CertificadosResource::class;
+
+    // Sobrescribir el método para redirigir al listado después de crear
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index'); // Redirige al listado
+    }
+
+    protected function getCreatedNotificationMessage(): ?string
+    {
+        return 'Certificado creado exitosamente'; // Mensaje personalizado
+    }
 }
--- app/Filament/Resources/ClientesResource/Pages/CreateClientes.php
+++ app/Filament/Resources/ClientesResource/Pages/CreateClientes.php
@@ -13,6 +13,17 @@ class CreateClientes extends CreateRecord
 {
     protected static string $resource = ClientesResource::class;
 
+    // Sobrescribir el método para redirigir al listado después de crear
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index'); // Redirige al listado
+    }
+
+    protected function getCreatedNotificationMessage(): ?string
+    {
+        return 'Cliente creado exitosamente'; // Mensaje personalizado
+    }
+
     public function table(Table $table): Table
     {
         return $table
