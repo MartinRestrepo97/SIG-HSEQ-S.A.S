a3804cd7301b1fbb6fe23bb606d8ff0796197cc7
MartinRestrepo97 <palaciosrestrepomartinernesto@gmail.com>
2024-12-29 17:48:54 -0500

    sig_hseq_sas_front_api

---
+++ app/Http/Controllers/Api/CertificadosController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Certificados;
+use Illuminate\Http\Request;
+
+class CertificadosController extends Controller
+{
+    public function index()
+    {
+        return Certificados::all();
+    }
+
+    public function store(Request $request)
+    {
+        return Certificados::create($request->all());
+    }
+
+    public function show($id)
+    {
+        return Certificados::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $certificado = Certificados::findOrFail($id);
+        $certificado->update($request->all());
+        return $certificado;
+    }
+
+    public function destroy($id)
+    {
+        Certificados::destroy($id);
+        return response()->noContent();
+    }
+}
